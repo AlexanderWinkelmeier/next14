95995efe034d69135b10857098c47f07c2a21d3d
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2024-03-16 15:38:46 +0100

    Active Class in the Navbar Link

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import styles from './links.module.css';
+import NavLink from './navLinks/navLink';
 const Links = () => {
   const links = [
     {
@@ -19,15 +19,22 @@ const Links = () => {
       path: '/blog',
     },
   ];
+
+  const session = true;
+  const isAdmin = true;
   return (
-    <div>
-      <div className={styles.links}>
-        {links.map((link) => (
-          <Link href={link.path} key={link.title}>
-            {link.title}
-          </Link>
-        ))}
-      </div>
+    <div className={styles.links}>
+      {links.map((link) => (
+        <NavLink key={link.title} item={link} />
+      ))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: 'Login', path: '/login' }} />
+      )}
     </div>
   );
 };
